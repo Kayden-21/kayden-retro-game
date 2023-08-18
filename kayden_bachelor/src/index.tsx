43996fafbbed392a4bf10fc8022fc3ea09a343fc
Line Dorfling <line@bbd.co.za>
2023-08-18 23:07:50 +0200

    make some parent folder

--- kayden_bachelor/src/index.tsx
+++ kayden_bachelor/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
-import { PageRouter } from "./pageRouter";
+import { PageRouter } from "./pages/pageRouter";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- kayden_bachelor/src/pages/pageRouter.tsx
+++ kayden_bachelor/src/pages/pageRouter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { LoginPage } from "./pages/loginPage";
-import { DashboardPage } from "./pages/dashboardPage";
+import { LoginPage } from "./LoginPage/loginPage";
+import { DashboardPage } from "./DashboardPage/dashboardPage";
 
 export const PageRouter = () => {
   return (
