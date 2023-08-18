40ed165c7bdfdcba029e25986d7adb16ca96a288
Line Dorfling <line@bbd.co.za>
2023-08-18 23:05:03 +0200

    cleaned up setup and routing

---
+++ kayden_bachelor/src/pageRouter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/loginPage";
+import { DashboardPage } from "./pages/dashboardPage";
+
+export const PageRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
+  );
+};
---
+++ kayden_bachelor/src/pages/loginPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/App.css";
+
+export const LoginPage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Login</h1>
+      </header>
+    </div>
+  );
+};
