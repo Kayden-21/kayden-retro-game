a4d25e9860f7504731fb5669cdc2474a05178154
Kayden Kara <kayden@bbd.co.za>
2023-08-21 14:39:54 +0200

    Added google auth

--- kayden_bachelor/src/pages/LoginPage/loginPage.tsx
+++ kayden_bachelor/src/pages/LoginPage/loginPage.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { LoginLayout } from "../../components/baseLayout/baseLayout";
 import { LoginForm } from "../../components/loginForm/loginForm";
+import { initializeGoogleAuth } from '../../utilities/GoogleAuth';
 
 export const LoginPage = () => {
+  useEffect(() => {
+    initializeGoogleAuth('148921217594-sc71tpp56j6ci623c2e11hagglavalb3.apps.googleusercontent.com');
+  }, []);
   return (
     <LoginLayout>
       <LoginForm heading="Login" buttonText="Login with Google" />
